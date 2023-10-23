c1b641248d654d10bcd20d4cc9452c5b45548177
Kelvin de Looff <kelvindelooff@gmail.com>
2023-10-23 16:19:44 +0200

    Login.tsx basic html gemaakt

--- ClientApp/src/pages/Login/Login.tsx
+++ ClientApp/src/pages/Login/Login.tsx
@@ -1,14 +1,64 @@
 import Layout from "../../components/Layout";
+import React, { useState } from 'react';
 
 import "./Login.scss";
 
-
-
 const Login = () => {
+
+    const [email, setEmail] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
 
     return (
         <Layout>
+            <div className="container mt-5">
+                <div className="row justify-content-center">
+                    <div className="col-md-6">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Sign In</h5>
+                                <form
+                                    onSubmit={e => {
+                                        e.preventDefault();
+                                        console.log({ email, password });
+                                    }}
+                                >
 
+                                    {/* Email Input */}
+                                    <div className="form-group">
+                                        <label htmlFor="email">Email</label>
+                                        <input
+                                            type="email"
+                                            className="form-control"
+                                            id="email"
+                                            placeholder="Enter your email"
+                                            required
+                                            value={email}
+                                            onChange={e => setEmail(e.target.value)}
+                                        />
+                                    </div>
+
+                                    {/* Password Input */}
+                                    <div className="form-group">
+                                        <label htmlFor="password">Password</label>
+                                        <input
+                                            type="password"
+                                            className="form-control"
+                                            id="password"
+                                            placeholder="Enter your password"
+                                            required
+                                            value={password}
+                                            onChange={e => setPassword(e.target.value)}
+                                        />
+                                    </div>
+
+                                    {/* Sign In Button */}
+                                    <button type="submit" className="btn btn-primary">Sign in</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Layout>
     );
 };
