f1fec5eacdcc1d25cd186e33dee666b197dd82ec
Kelvin de Looff <kelvindelooff@gmail.com>
2023-10-23 14:08:21 +0200

    Inlog.tsx en .scss aangemaakt en in App.tsx een refrence ernaar toegevoegt.

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -5,21 +5,23 @@ import About from "./pages/About/About";
 import Forum from "./pages/Forum/Forum";
 import Activities from "./pages/Activities/Activities";
 import Profile from "./pages/Profile/Profile";
+import Login from "./pages/Login/Login";
 
 const App = () => {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/forum" element={<Forum />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/activities" element={<Activities />} />
-        </Routes>
-      </Router>
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/forum" element={<Forum />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/activities" element={<Activities />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </Router>
+        </>
+    );
 };
 
 export default App;
---
+++ ClientApp/src/pages/Login/Login.tsx
@@ -0,0 +1,16 @@
+import Layout from "../../components/Layout";
+
+import "./Login.scss";
+
+
+
+const Login = () => {
+
+    return (
+        <Layout>
+
+        </Layout>
+    );
+};
+
+export default Login;
