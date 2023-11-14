ec5668f3da0e7df2f1beb7c170bcaff299b40eb3
Kelvin de Looff <47848591+Pankrazzz@users.noreply.github.com>
2023-11-14 16:24:12 +0100

    Inlogpagina (#4)
    * Inlog.tsx en .scss aangemaakt en in App.tsx een refrence ernaar toegevoegt.
    
    * Login.tsx basic html gemaakt
    
    * Design inlogpagina verbeterd
    
    :)
    
    * inlogpagina scss veranderd
    
    Margin met bootstrap gegeven, titel kleur veranderd.
    
    * loginpagina aan header/nav menu toegevoegt

--- ClientApp/src/components/NavMenu.tsx
+++ ClientApp/src/components/NavMenu.tsx
@@ -7,35 +7,40 @@ const LogoWhite = require("../assets/logo-white.png");
 const profile = require("../assets/profile-icon.png");
 
 const NavMenu = () => {
-  return (
-    <Navbar className="navbar navbar-expand-lg antes-red p-4">
-      <NavbarBrand tag={Link} to="/">
-        <img className="navbar-logo" src={LogoWhite} alt="Logo" />
-      </NavbarBrand>
-      <ul className="navbar-nav">
-        <NavItem>
-          <NavLink tag={Link} className="text-light fs-3" to="/activities">
-            Activiteiten
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink tag={Link} className="text-light fs-3 ms-3" to="/about">
-            Over ons
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink tag={Link} className="text-light fs-3 ms-3" to="/forum">
-            Forum
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavbarBrand tag={Link} to="/profile">
-            <img className="profile-image" src={profile} alt="profiel" />
-          </NavbarBrand>
-        </NavItem>
-      </ul>
-    </Navbar>
-  );
+    return (
+        <Navbar className="navbar navbar-expand-lg antes-red p-4">
+            <NavbarBrand tag={Link} to="/">
+                <img className="navbar-logo" src={LogoWhite} alt="Logo" />
+            </NavbarBrand>
+            <ul className="navbar-nav">
+                <NavItem>
+                    <NavLink tag={Link} className="text-light fs-3" to="/activities">
+                        Activiteiten
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink tag={Link} className="text-light fs-3 ms-3" to="/about">
+                        Over ons
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink tag={Link} className="text-light fs-3 ms-3" to="/forum">
+                        Forum
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink tag={Link} className="text-light fs-3 ms-3" to="/login">
+                        Inloggen
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavbarBrand tag={Link} to="/profile">
+                        <img className="profile-image" src={profile} alt="profiel" />
+                    </NavbarBrand>
+                </NavItem>
+            </ul>
+        </Navbar>
+    );
 };
 
 export default NavMenu;
