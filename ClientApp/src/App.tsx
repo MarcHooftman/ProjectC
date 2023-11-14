dd88fc9b47827fbd94cdb7841acdf16fad20995b
Kelvin de Looff <47848591+Pankrazzz@users.noreply.github.com>
2023-11-14 16:25:18 +0100

    Profiel bewerk + footer (nog niet responsive) (#6)
    * Edit_profile toegevoegt (Read description)
    
    2 files aangemaakt (edit_profile.tsx/scss), Profile Interface uitgebreid, PersonalInfoCard.tsx haalt bio van profiel af en heb telefoonnummer toegevoegt (Als telefoonnummer leeg is wordt het niet displayed) en department toegevoegt, heb een button onderaan profile.tsx gezet die redirect naar de bewerk pagina. Heb refrence toegevoegt naar de edit pagina in de App.tsx. Heel de edit pagina aangemaakt en ingevult met nodige content.
    
    * Naam, email (misschien profielfoto, department) mag niet verandert worden in edit_profile.tsx..
    
    * Footer gemaakt :(
    
    ---------
    
    Co-authored-by: MarcHooftman <124693465+MarcHooftman@users.noreply.github.com>

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -5,6 +5,7 @@ import About from "./pages/About/About";
 import Forum from "./pages/Forum/Forum";
 import Activities from "./pages/Activities/Activities";
 import Profile from "./pages/Profile/Profile";
+import ProfileEdit from "./pages/Profile/Edit_Profile/edit_profile";
 import Login from "./pages/Login/Login";
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
                     <Route path="/about" element={<About />} />
                     <Route path="/forum" element={<Forum />} />
                     <Route path="/profile" element={<Profile />} />
+                    <Route path="/edit_profile" element={<ProfileEdit />} />
                     <Route path="/activities" element={<Activities />} />
                     <Route path="/login" element={<Login />} />
                 </Routes>
