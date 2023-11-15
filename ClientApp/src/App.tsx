3068cb37ee776d8f8a4c76c80080ad4f00c19042
Kelvin de Looff <47848591+Pankrazzz@users.noreply.github.com>
2023-11-15 14:49:52 +0100

    Footer responsive gemaakt + 404 toegevoegd (#7)
    * Edit_profile toegevoegt (Read description)
    
    2 files aangemaakt (edit_profile.tsx/scss), Profile Interface uitgebreid, PersonalInfoCard.tsx haalt bio van profiel af en heb telefoonnummer toegevoegt (Als telefoonnummer leeg is wordt het niet displayed) en department toegevoegt, heb een button onderaan profile.tsx gezet die redirect naar de bewerk pagina. Heb refrence toegevoegt naar de edit pagina in de App.tsx. Heel de edit pagina aangemaakt en ingevult met nodige content.
    
    * Naam, email (misschien profielfoto, department) mag niet verandert worden in edit_profile.tsx..
    
    * Footer gemaakt :(
    
    * installed react-bootstrap
    
    * Footer fancy gemaakt op mobiele devices
    
    * 404 gemaakt
    
    ---------
    
    Co-authored-by: MarcHooftman <124693465+MarcHooftman@users.noreply.github.com>

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import NotFound from "./pages/NotFound"
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Forum from "./pages/Forum/Forum";
@@ -9,11 +10,13 @@ import Training from "./pages/Training/Training";
 import ProfileEdit from "./pages/Profile/Edit_Profile/edit_profile";
 import Login from "./pages/Login/Login";
 
+
 const App = () => {
     return (
         <>
             <Router>
                 <Routes>
+                    <Route path='*' element={<NotFound />} />
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/forum" element={<Forum />} />
