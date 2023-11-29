3ef0a9b77c9df04eefaa94c5dc2d6f342059b28a
MarcHooftman <1064802@hr.nl>
2023-11-29 19:01:00 +0100

    like functionality

--- ClientApp/src/interfaces/IForumPost.tsx
+++ ClientApp/src/interfaces/IForumPost.tsx
@@ -2,14 +2,14 @@ import ILike from "./ILike";
 import IProfile from "./IProfile";
 
 export default interface IForumPost {
-    id: number;
-    title: string;
-    content: string;
-    tags: { id: number; name: string; }[];
-    profile: IProfile;
-    time: string;
-    forumPostID: number;
-    comments: IForumPost[];
-    likes: ILike[];
-    reports: IProfile[];
+  id?: number;
+  title: string;
+  content: string;
+  tags: { id: number; name: string }[];
+  profile: IProfile;
+  time: string;
+  forumPostID: number;
+  comments: IForumPost[];
+  likes: ILike[];
+  reports: IProfile[];
 }
--- ClientApp/src/pages/Forum/ForumPostCard.tsx
+++ ClientApp/src/pages/Forum/ForumPostCard.tsx
@@ -1,5 +1,13 @@
 import { useEffect, useRef, useState } from "react";
-import { Badge, Button, Card, Col, Row } from "react-bootstrap";
+import {
+  Badge,
+  Button,
+  Card,
+  Col,
+  Overlay,
+  Popover,
+  Row,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import IForumPost from "../../interfaces/IForumPost";
 import PostComment from "./PostComment";
@@ -10,146 +18,214 @@ import ReportIcon from "../../assets/report.svg";
 import useGraphData from "../../hooks/useGraphData";
 import useFetch from "../../hooks/useFetch";
 import IProfile from "../../interfaces/IProfile";
+import ILike from "../../interfaces/ILike";
+import { Tooltip } from "react-bootstrap";
 
 const upArrow = require("../../assets/up-arrow.png");
-const downArrow = require('../../assets/down-arrow.png');
+const downArrow = require("../../assets/down-arrow.png");
 const profilePicture = require("../../assets/profile.png");
 
-
 interface Props {
-    post?: IForumPost;
+  post?: IForumPost;
 }
 
 const ForumPostCard = ({ post }: Props) => {
-    const [collapse, setCollapse] = useState<boolean>(true);
-    const [liked, setLiked] = useState(false);
-    const [localPost, setLocalPost] = useState<IForumPost | undefined>(post);
+  const [collapse, setCollapse] = useState<boolean>(true);
+  const [liked, setLiked] = useState(false);
+  const [initialize, setInitialize] = useState<boolean>(true);
 
-    const { graphData } = useGraphData();
-    const { data } = useFetch(`https://localhost:7185/api/profile/by-email/${graphData?.mail}`);
+  const ref = useRef(null);
+  const [show, setShow] = useState(false);
 
-    console.log(localPost)
+  const { graphData } = useGraphData();
+  const { data } = useFetch(
+    `https://localhost:7185/api/profile/by-email/${graphData?.mail}`
+  );
 
-    useEffect(() => {
-        if (localPost?.likes && localPost?.likes.length > 0) {
-            localPost?.likes.forEach(like => {
-                if (like.profileID === (data as IProfile)?.id) {
-                    setLiked(true);
-                }
-            })
+  //console.log(post)
+
+  useEffect(() => {
+    if (post?.likes && post?.likes.length > 0 && initialize) {
+      post?.likes.forEach((like) => {
+        if (like.profileID === (data as IProfile)?.id) {
+          setLiked(true);
+          setInitialize(false);
         }
-
-    })
-
-    const RefreshPost = () => {
-        fetch(`https://localhost:7185/api/forumpost/${localPost?.id}`)
-            .then((response) => response.json())
-            .then((json) => {
-                if (json.traceId) {
-                    if (json.status && json.title) {
-                        console.log(`Error: ${json.status} - ${json.title}, traceId: ${json.traceId}`);
-                    }
-                    console.log("Error: Unknown error");
-                } else {
-                    setLocalPost(json);
-                }
-            })
-            .catch((error) => console.log(error))
+      });
     }
+  });
 
-    const handleLike = () => {
+  const handleLike = () => {
+    setLiked(!liked);
 
-        setLiked(!liked);
+    if (!liked) {
+      const like = {
+        ForumPostID: post?.id,
+        ProfileID: (data as IProfile).id,
+      };
 
-        if (!liked) {
-            const like = {
-                ForumPostID: localPost?.id,
-                ProfileID: (data as IProfile).id,
-            }
-
-            fetch("https://localhost:7185/api/like", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(like),
-            })
-        } else {
-            const like = localPost?.likes.find(like => like.profileID === data.id);
-            fetch(`https://localhost:7185/api/like/${like?.id}`, {
-                method: "DELETE",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            })
-        }
-
-        RefreshPost();
+      fetch("https://localhost:7185/api/like", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(like),
+      });
+    } else {
+      const findLike = (likeArray: ILike[]) =>
+        likeArray.find((like) => like.profileID === data.id);
+      fetch(`https://localhost:7185/api/forumpost/${post?.id}`)
+        .then((response) => response.json())
+        .then((data) =>
+          fetch(`https://localhost:7185/api/like/${findLike(data.likes)?.id}`, {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          })
+        );
     }
+  };
 
-    const handleComment = () => {
-        console.log("comment")
+  const handleReport = () => {
+    console.log("report");
+  };
 
+  const [comment, setComment] = useState<string>();
+  const handleCommentChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setComment(e.target.value);
+  };
+  const handleCommentSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (post && post.id) {
+      const commentObject: IForumPost = {
+        title: "",
+        content: comment || "",
+        tags: [],
+        profile: data as IProfile,
+        time: new Date().toISOString(),
+        forumPostID: post.id,
+        comments: [],
+        likes: [],
+        reports: [],
+      };
+
+      fetch("https://localhost:7185/api/forumpost", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(commentObject),
+      });
+
+      fetch(`https://localhost:7185/api/forumpost/${post.id}`)
+        .then((response) => response.json())
+        .then((data) => (post = data as IForumPost));
     }
+  };
 
-    const handleReport = () => {
-        console.log("report")
-
+  useEffect(() => {
+    if (show) {
+      (ref.current as HTMLTextAreaElement | null)?.focus();
     }
+  }, [show]);
 
-    let formattedDate = "";
+  let formattedDate = "";
 
-    if (localPost?.time !== undefined) {
-        formattedDate = new Date(localPost.time).toLocaleString("nl-NL", { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric' });
-    }
+  if (post?.time !== undefined) {
+    formattedDate = new Date(post.time).toLocaleString("nl-NL", {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+    });
+  }
 
-    return (
-        <Card as="details" className="my-3 shadow-lg">
-            <Card.Header as="summary" className="d-flex align-items-center" onClick={() => setCollapse(!collapse)}>
-                <Link to="/profile">
-                    <Card.Img src={profilePicture} className="poster-pfp"></Card.Img>
-                </Link>
-                <Row className="w-100 align-items-center">
-                    <Col lg={3} className="ps-4"><h2 className="fs-5 m-0">
-                        <strong>{localPost?.profile?.fullName}</strong>
-                    </h2>
-                        <h3 className="fs-6 m-0 opacity-50 text-dark">
-                            lid sinds {localPost?.profile?.memberSince}
-                        </h3>
-                    </Col>
-                    <Col lg={6} className="ps-4">
-                        <h4>{localPost?.title}</h4>
-                    </Col>
+  return (
+    <Card as="details" className="my-3 shadow-lg">
+      <Card.Header
+        as="summary"
+        className="d-flex align-items-center"
+        onClick={() => setCollapse(!collapse)}
+      >
+        <Link to="/profile">
+          <Card.Img src={profilePicture} className="poster-pfp"></Card.Img>
+        </Link>
+        <Row className="w-100 align-items-center">
+          <Col lg={3} className="ps-4">
+            <h2 className="fs-5 m-0">
+              <strong>{post?.profile?.fullName}</strong>
+            </h2>
+            <h3 className="fs-6 m-0 opacity-50 text-dark">
+              lid sinds {post?.profile?.memberSince}
+            </h3>
+          </Col>
+          <Col lg={6} className="ps-4">
+            <h4>{post?.title}</h4>
+          </Col>
 
-                    <Col lg={2} className="ps-5">
-                        <span className="opacity-50 text-dark">{formattedDate}</span>
-                    </Col>
-                    <Col lg={1} className="ps-5">
-                        <img className="arrow-icon" src={collapse ? upArrow : downArrow} />
-                    </Col>
-                </Row>
-            </Card.Header>
-            <Card.Body>
-                {localPost?.content}
-                <span className="d-flex gap-2 mt-2">
-                    {localPost?.tags && Array.isArray(localPost?.tags) && localPost?.tags.map(tag => <Link to={`/forum?filter=${tag}`}><Badge className="badge-color" text="light" pill={true}>{tag.name}</Badge></Link>)}
-                </span>
-            </Card.Body>
-            <Card.Footer>
-                <div className="mb-3 d-flex gap-4">
-                    <img src={liked ? LikeFillIcon : LikeIcon} className="action-icon" onClick={handleLike} />
-                    <img src={CommentIcon} className="action-icon" onClick={handleComment} />
-                    <img src={ReportIcon} className="action-icon" onClick={handleReport} />
-                </div>
+          <Col lg={2} className="ps-5">
+            <span className="opacity-50 text-dark">{formattedDate}</span>
+          </Col>
+          <Col lg={1} className="ps-5">
+            <img className="arrow-icon" src={collapse ? upArrow : downArrow} />
+          </Col>
+        </Row>
+      </Card.Header>
+      <Card.Body>
+        {post?.content}
+        <span className="d-flex gap-2 mt-2">
+          {post?.tags &&
+            Array.isArray(post?.tags) &&
+            post?.tags.map((tag) => (
+              <Link to={`/forum?filter=${tag}`}>
+                <Badge className="badge-color" text="light" pill={true}>
+                  {tag.name}
+                </Badge>
+              </Link>
+            ))}
+        </span>
+      </Card.Body>
+      <Card.Footer>
+        <div className="mb-3 d-flex gap-4">
+          <img
+            src={liked ? LikeFillIcon : LikeIcon}
+            className="action-icon"
+            onClick={handleLike}
+          />
+          <img
+            src={CommentIcon}
+            className="action-icon"
+            onClick={() => setShow(!show)}
+          />
+          <img
+            src={ReportIcon}
+            className="action-icon"
+            onClick={handleReport}
+          />
+        </div>
+        <form onSubmit={handleCommentSubmit}>
+          <input
+            ref={ref}
+            className="position-relative box mb-3"
+            style={show ? { display: "block" } : { display: "none" }}
+            onChange={handleCommentChange}
+          />
+          <Button type="submit">Plaats</Button>
+        </form>
 
-                {localPost?.comments && localPost?.comments.length > 0
-                    ? localPost?.comments.map(comment => <PostComment key={comment.id} comment={comment}></PostComment>)
-                    : <span className="opacity-50 text-dark">No comments</span>
-                }
-
-            </Card.Footer>
-        </Card>
-    )
-}
+        {post?.comments && post?.comments.length > 0 ? (
+          post?.comments.map((comment) => (
+            <PostComment key={comment.id} comment={comment}></PostComment>
+          ))
+        ) : (
+          <span className="opacity-50 text-dark">No comments</span>
+        )}
+      </Card.Footer>
+    </Card>
+  );
+};
 
 export default ForumPostCard;
