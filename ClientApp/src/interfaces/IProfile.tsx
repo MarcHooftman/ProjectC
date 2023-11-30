685974430a3fd2f623f8953173463e822aa55876
Kelvin de Looff <47848591+Pankrazzz@users.noreply.github.com>
2023-11-30 14:08:59 +0100

    Profiel Updaten ☜(ﾟヮﾟ☜) (#10)
    * Update edit_profile.tsx
    
    * graphdata toegevoegt
    
    * Data wordt opgeslagen in DB
    
    Moet nog bug fixen met form invullen.
    
    * Kan profile updaten
    
    Heb IProfile aangepast, geeft overal errors maar werkt wel??? Idk
    
    * IProfile aangepast, Profiel bewerk werkt.
    
    * Redirect toegevoegt

--- ClientApp/src/interfaces/IProfile.tsx
+++ ClientApp/src/interfaces/IProfile.tsx
@@ -1,19 +1,18 @@
 // import { Media } from "reactstrap";
 
 export default interface IProfile {
-  id?: number;
-  fullName: string;
-  role: string;
-  dateOfBirth: string;
-  email: string;
-  memberSince: string;
-  phoneNumber: string | null;
-  lastLogin: string | null;
-  bio: string;
-  department: string;
-  user: {
-    id: number;
+    id?: number;
+    fullName: string;
+    role: string;
+    dateOfBirth: string;
     email: string;
-  };
-  // ProfilePicture: Media;
+    memberSince: string;
+    phoneNumber: string | null;
+    bio: string;
+    department: string;
+    user: {
+        id: number;
+        email: string;
+    }
+    // ProfilePicture: Media;
 }
