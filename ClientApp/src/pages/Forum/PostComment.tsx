f6abb6817f04fa478928f2dc2604391494eeb81e
MarcHooftman <1064802@hr.nl>
2023-12-05 20:32:36 +0100

    seeding (half)

--- ClientApp/src/pages/Forum/PostComment.tsx
+++ ClientApp/src/pages/Forum/PostComment.tsx
@@ -8,7 +8,7 @@ import IProfile from "../../interfaces/IProfile";
 import LikeButton from "./ForumPostButtons/LikeButton";
 import CommentButton from "./ForumPostButtons/CommentButton";
 import ReportButton from "./ForumPostButtons/ReportButton";
-import { Button } from "react-bootstrap";
+import { Badge, Button } from "react-bootstrap";
 
 const profilePicture = require("../../assets/profile.png");
 
@@ -17,7 +17,7 @@ interface Props {
   onClick?: () => void;
 }
 
-const PostComment = ({ onClick = () => { }, comment }: Props) => {
+const PostComment = ({ onClick = () => {}, comment }: Props) => {
   const ref = useRef<any>(null);
   const [show, setShow] = useState(false);
 
@@ -50,7 +50,9 @@ const PostComment = ({ onClick = () => { }, comment }: Props) => {
   const [profile, setProfile] = useState<IProfile>();
   useEffect(() => {
     if (graphData) {
-      fetch(`${process.env.REACT_APP_API_URL}/profile/by-email/${graphData?.mail}`)
+      fetch(
+        `${process.env.REACT_APP_API_URL}/profile/by-email/${graphData?.mail}`
+      )
         .then((response) => response.json())
         .then((data) => setProfile(data as IProfile));
     }
@@ -106,17 +108,37 @@ const PostComment = ({ onClick = () => { }, comment }: Props) => {
           <p className="text-dark opacity-50 mb-0">
             {formatDate(comment.time)}
           </p>
-          {comment?.id && profile?.id &&
+          {comment?.id && profile?.id && (
             <div className="d-flex gap-2">
-              <LikeButton postId={comment.id} profileId={profile.id} onClick={onClick} />
+              <LikeButton
+                postId={comment.id}
+                profileId={profile.id}
+                onClick={onClick}
+              />
               <CommentButton inputRef={ref} onClick={handleCommentClick} />
-              <ReportButton postId={comment.id} profileId={profile.id} onClick={onClick} />
+              <ReportButton
+                postId={comment.id}
+                profileId={profile.id}
+                onClick={onClick}
+              />
             </div>
-          }
+          )}
         </div>
         <p style={{ marginLeft: "35px", marginBottom: "0px" }}>
           {comment.content}
+          <span className="d-flex gap-2">
+            {comment?.tags &&
+              Array.isArray(comment?.tags) &&
+              comment?.tags.map((tag) => (
+                <Link to={`/forum?filter=${tag.name}`}>
+                  <Badge className="badge-color" text="light" pill={true}>
+                    {tag.name}
+                  </Badge>
+                </Link>
+              ))}
+          </span>
         </p>
+
         <form
           onSubmit={handleCommentSubmit}
           className=" border-secondary ms-2 border-start ps-3"
@@ -143,7 +165,6 @@ const PostComment = ({ onClick = () => { }, comment }: Props) => {
       )}
     </div>
   );
-
 };
 
 export default PostComment;
---
+++ Seeder.cs
@@ -0,0 +1,353 @@
+// using API.Models;
+
+// // namespace API.Models;
+
+// // public class Profile
+// // {
+// //     public int ID { get; set; }
+// //     public string Email { get; set; } = null!;
+// //     public string? FullName { get; set; }
+// //     public string? Bio { get; set; }
+// //     public DateOnly? MemberSince { get; set; }
+// //     public DateTime? LastLogin { get; set; }
+// //     public string? Role { get; set; }
+// //     public DateOnly? DateOfBirth { get; set; }
+// //     public string? Department { get; set; }
+// //     public int? ProfilePictureID { get; set; }
+// //     public Media? ProfilePicture { get; set; }
+// //     public string? PhoneNumber { get; set; }
+// //     public string? UserPrincipalName { get; set; }
+
+// //     // navigational
+// //     public IEnumerable<Training>? Training { get; set; }
+// // }
+
+// // namespace API.Models;
+
+// // public class Activity
+// // {
+// //     public int ID { get; set; }
+// //     public string Title { get; set; } = null!;
+// //     public string Description { get; set; } = null!;
+// //     public string Location { get; set; } = null!;
+// //     public DateTime Time { get; set; }
+// // }
+
+// // namespace API.Models;
+
+// // public class ForumPost
+// // {
+// //     public int ID { get; set; }
+// //     public string Title { get; set; } = null!;
+// //     public string Content { get; set; } = null!;
+// //     public IEnumerable<Tag>? Tags { get; set; }
+// //     public int ProfileID { get; set; }
+// //     public Profile? Profile { get; set; }
+// //     public DateTime Time { get; set; }
+// //     public int? ForumPostID { get; set; }
+// //     public IEnumerable<ForumPost>? Comments { get; set; }
+// //     public IEnumerable<Like>? Likes { get; set; }
+// //     public IEnumerable<Report>? Reports { get; set; }
+// // }
+
+
+
+// public static class Seeder
+// {
+//     public static void Initialize(AntesContext context)
+//     {
+//         context.Database.EnsureCreated();
+
+//         // Seed data
+//         AddProfiles(context);
+//     }
+
+//     private static void AddProfiles(AntesContext context) 
+//     {
+//         if (context.Profile.Any()) return;
+        
+//         var profiles = new Profile[]
+//         {
+//             new Profile{
+//                 Email="1000000@hr.nl",
+//                 FullName="Klaas Klaassen",
+//                 Bio="I'm a student at Hogeschool Rotterdam",
+//                 MemberSince=new DateOnly(2021, 1, 1),
+//                 LastLogin=DateTime.Now,
+//                 Role="User",
+//                 DateOfBirth=new DateOnly(2001, 1, 1),
+//                 Department="ICT",
+//                 PhoneNumber="0612345678",
+//                 UserPrincipalName="1000000@hr.nl",
+//             },
+//             new Profile{
+//                 Email="1000001@hr.nl",
+//                 FullName="Jan Janssen",
+//                 Bio="I'm a student at Hogeschool Rotterdam",
+//                 MemberSince=new DateOnly(2021, 1, 1),
+//                 LastLogin=DateTime.Now,
+//                 Role="User",
+//                 DateOfBirth=new DateOnly(2001, 1, 1),
+//                 Department="ICT",
+//                 PhoneNumber="0612345678",
+//                 UserPrincipalName="1000001@hr.nl",
+//             },
+//             new Profile{
+//                 Email="1000002@hr.nl",
+//                 FullName="Frank Frankken",
+//                 Bio="I'm a student at Hogeschool Rotterdam",
+//                 MemberSince=new DateOnly(2021, 1, 1),
+//                 LastLogin=DateTime.Now,
+//                 Role="User",
+//                 DateOfBirth=new DateOnly(2001, 1, 1),
+//                 Department="ICT",
+//                 PhoneNumber="0612345678",
+//                 UserPrincipalName="1000002@hr.nl",
+//             },
+//         };
+
+//         foreach (Profile profile in profiles)
+//         {
+//             context.Profile.Add(profile);
+//         }
+
+//         context.SaveChanges();
+//     }
+
+// namespace API.Models;
+
+// public class Tag
+// {
+//     public int ID { get; set; }
+//     public string Name { get; set; } = null!;
+// }
+
+using API.Models;
+using System;
+using System.Collections.Generic;
+
+namespace API;
+
+public static class Seeder
+{
+    public static void Initialize(AntesContext context)
+    {
+        context.Database.EnsureCreated();
+
+        AddProfiles(context);
+        AddActivities(context);
+        AddForumPosts(context);
+        AddTags(context);
+    }
+
+    private static void AddProfiles(AntesContext context) 
+    {
+        if (context.Profile.Any()) return;
+        
+        var profiles = new Profile[]
+        {
+            new()
+            {
+                Email="1000000@hr.nl",
+                FullName="Klaas Klaassen",
+                Bio="I'm a student at Hogeschool Rotterdam",
+                MemberSince=new DateOnly(2021, 1, 1),
+                LastLogin=DateTime.Now,
+                Role="User",
+                DateOfBirth=new DateOnly(2001, 1, 1),
+                Department="ICT",
+                PhoneNumber="0612345678",
+                UserPrincipalName="1000000@hr.nl",
+            },
+            new()
+            {
+                Email="1000001@hr.nl",
+                FullName="Jan Janssen",
+                Bio="I'm a student at Hogeschool Rotterdam",
+                MemberSince=new DateOnly(2021, 1, 1),
+                LastLogin=DateTime.Now,
+                Role="User",
+                DateOfBirth=new DateOnly(2001, 1, 1),
+                Department="ICT",
+                PhoneNumber="0612345678",
+                UserPrincipalName="1000001@hr.nl",
+            },
+            new()
+            {
+                Email="1000002@hr.nl",
+                FullName="Frank Frankken",
+                Bio="I'm a student at Hogeschool Rotterdam",
+                MemberSince=new DateOnly(2021, 1, 1),
+                LastLogin=DateTime.Now,
+                Role="User",
+                DateOfBirth=new DateOnly(2001, 1, 1),
+                Department="ICT",
+                PhoneNumber="0612345678",
+                UserPrincipalName="1000002@hr.nl",
+            },
+        };
+
+        foreach (Profile profile in profiles)
+        {
+            context.Profile.Add(profile);
+        }
+
+        context.SaveChanges();
+    }
+
+    private static void AddActivities(AntesContext context)
+    {
+        if (context.Activity.Any()) return;
+        var activities = new List<Activity>
+        {
+            new()
+            {
+                Title = "First Activity",
+                Description = "This is the first activity.",
+                Location = "Rotterdam",
+                Time = DateTime.Now
+            },
+            new()
+            {
+                Title = "Second Activity",
+                Description = "This is the second activity.",
+                Location = "Rotterdam",
+                Time = DateTime.Now
+            }
+        };
+
+        context.Activity.AddRange(activities);
+        context.SaveChanges();
+    }
+
+    private static void AddForumPosts(AntesContext context)
+    {
+        if (context.ForumPost.Any()) return;
+        var forumPosts = new List<ForumPost>
+        {
+            new()
+            {
+                Title = "First Post",
+                Content = "This is the first post.",
+                Tags = new List<Tag>
+                {
+                    new()
+                    {
+                        Name = "Tag 1"
+                    },
+                    new()
+                    {
+                        Name = "Tag 2"
+                    },
+                    new()
+                    {
+                        Name = "Tag 3"
+                    }
+                },
+                ProfileID = 1,
+                Profile = null,
+                Time = DateTime.Now,
+                ForumPostID = null,
+                Comments = new List<ForumPost>
+                {
+                    new()
+                    {
+                        Title = "Comment 1",
+                        Content = "This is a comment on the first post.",
+                        Tags = new List<Tag>
+                        {
+                            new()
+                            {
+                                Name = "Tag 1"
+                            }
+                        },
+                        ProfileID = 2,
+                        Profile = null,
+                        Time = DateTime.Now,
+                        ForumPostID = 1,
+                        Comments = new List<ForumPost>
+                        {
+                            new()
+                            {
+                                Title = "Sub Comment 1",
+                                Content = "This is a sub comment on the firstcomment.",
+                                Tags = new List<Tag>
+                                {
+                                    new()
+                                    {
+                                        Name = "Tag 2"
+                                    }
+                                },
+                                ProfileID = 3,
+                                Profile = null,
+                                Time = DateTime.Now,
+                                ForumPostID = 2,
+                                Comments = new List<ForumPost>(),
+                                Likes = new List<Like>(),
+                                Reports = new List<Report>()
+                            }
+                        },
+                        Likes = new List<Like>(),
+                        Reports = new List<Report>()
+                    },
+                    new()
+                    {
+                        Title = "Comment 2",
+                        Content = "This is another comment on the first post.",
+                        Tags = new List<Tag>(),
+                        ProfileID = 3,
+                        Profile = null,
+                        Time = DateTime.Now,
+                        ForumPostID = 1,
+                        Comments = new List<ForumPost>(),
+                        Likes = new List<Like>(),
+                        Reports = new List<Report>()
+                    }
+                },
+                Likes = new List<Like>(),
+                Reports = new List<Report>()
+            },
+            new()
+            {
+                Title = "Second Post",
+                Content = "This is the second post.",
+                Tags = new List<Tag>(),
+                ProfileID = 2,
+                Profile = null,
+                Time = DateTime.Now,
+                ForumPostID = null,
+                Comments = new List<ForumPost>(),
+                Likes = new List<Like>(),
+                Reports = new List<Report>()
+            }
+        };
+
+        context.ForumPost.AddRange(forumPosts);
+        context.SaveChanges();
+    }
+
+    private static void AddTags(AntesContext context)
+    {
+        if (context.Tag.Any()) return;
+        var tags = new List<Tag>
+        {
+            new()
+            {
+                Name = "Tag 1"
+            },
+            new()
+            {
+                Name = "Tag 2"
+            },
+            new()
+            {
+                Name = "Tag 3"
+            }
+        };
+
+        context.Tag.AddRange(tags);
+        context.SaveChanges();
+    }
+}
+
+
