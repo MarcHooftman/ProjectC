a50ac6050dc95a61965d327ee3a8102cdd64466f
Ronald98kj <124693284+Ronald98kj@users.noreply.github.com>
2023-12-07 13:32:02 +0100

    Post forum op forum pagina

--- ClientApp/src/pages/Forum/Forum.tsx
+++ ClientApp/src/pages/Forum/Forum.tsx
@@ -2,7 +2,8 @@ import ForumPostCard from "./ForumPostCard";
 import Layout from "../../components/Layout";
 import FilterDropdown from "./FilterDropdown";
 import { useSearchParams } from "react-router-dom";
-
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import "./Forum.scss";
 import IForumPost from "../../interfaces/IForumPost";
 import { useEffect, useState } from "react";
@@ -12,6 +13,7 @@ const Forum = () => {
   const [forumPosts, setForumPosts] = useState<IForumPost[]>([]);
   const [searchParams] = useSearchParams();
   const filter = searchParams.get("filter");
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}/forumpost`)
@@ -24,13 +26,18 @@ const Forum = () => {
     posts = filter ? filterByTag(posts, filter) : posts;
     return sortByDate(filterOnlyParent(posts));
   };
-
+  const handleCreatePost = () => {
+    navigate('/post');
+  };
 
   return (
     <Layout>
       <span className="forum-header d-flex justify-content-between align-items-center">
         <h1 className="my-5 blue-text">Antes Forum</h1>
-        <FilterDropdown />
+        <div>
+          <FilterDropdown />
+          <Button variant="primary" onClick={handleCreatePost}>Post nieuwe form</Button> {/* New Post Button */}
+        </div>
       </span>
       {filterPosts(forumPosts).length > 0 ? (
         filterPosts(forumPosts).map((forumPost) => (
