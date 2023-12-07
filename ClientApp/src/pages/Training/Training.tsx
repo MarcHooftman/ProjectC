3b08e322e3d7e379172dbd28456e16bfc8f65ae9
RSHussain <rizwanhussain2801@gmail.com>
2023-12-07 22:45:50 +0100

    in progress Training profile progress

--- ClientApp/src/pages/Training/Training.tsx
+++ ClientApp/src/pages/Training/Training.tsx
@@ -1,34 +1,65 @@
 import Layout from "../../components/Layout";
 import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import ITraining from "../../interfaces/ITraining";
 import TrainingInfoCard from "./TrainingInfoCard";
 import "./Training.scss"
 import ITag from "../../interfaces/ITag";
 import { filterByTag } from "../../utils/sortTraining";
 import FilterDropdown from "../../components/FilterDropdown";
+import useGraphData from "../../hooks/useGraphData";
+import IProfile from "../../interfaces/IProfile";
+import { useIsAuthenticated, useMsal } from "@azure/msal-react";
+import { loginRequest } from "../../authConfig";
 
 const Training = () => {
+    const loggedIn = useIsAuthenticated();
+    const { instance, accounts } = useMsal();
+
+    const handleLogin = () => {
+        instance.loginRedirect(loginRequest).catch((e) => {
+        console.log(e);
+        });
+    };
+    if (!loggedIn) {
+        handleLogin();
+    }
+    
+    const { graphData } = useGraphData();
+    const [profile, setProfile] = useState<IProfile>();
+    useEffect(() => {
+        console.log(graphData)
+        if (graphData) {
+            fetch(
+                `${process.env.REACT_APP_API_URL}/profile/by-email/${graphData?.mail}`
+                )
+                .then((response) => response.json())
+                .then((data) => setProfile(data as IProfile));
+            }
+        }, [graphData]);
+        
     const [Trainings, setTraining] = useState<ITraining[]>([]);
     const [searchParams] = useSearchParams()
     const filter = searchParams.get('filter')
     let Category: ITag[] = []
-
-
+    
+    
     useEffect(() => {
         fetch(`${process.env.REACT_APP_API_URL}/training`)
-          .then((response) => response.json())
-          .then((data) => setTraining(data as ITraining[]));
-      }, [filter]);
-
+        .then((response) => response.json())
+        .then((data) => setTraining(data as ITraining[]));
+    }, [filter]);
+    
     const filterTraining = (training: ITraining[]) => {
-    training = filter ? filterByTag(training, filter) : training;
-    return training;
+        training = filter ? filterByTag(training, filter) : training;
+        return training;
     };
-
+    
+    console.log(profile)
+    console.log(Trainings)
     // sets category to every first tag of trainings
     filterTraining(Trainings)?.forEach((i) => { if (!Category.some(_ => _.name.includes(i.tags[0].name))) Category.push(i.tags[0]) })
-
+    
     return (
         <Layout>
             <span className="forum-header d-flex justify-content-between align-items-center">
@@ -39,7 +70,8 @@ const Training = () => {
                 return <div key={i.id} className="ps-3">
                     <h3 className="pt-3 blue-text" >{i.name}</h3>
                     <div className="d-flex pt-3 gap-4 flex-row flex-wrap justify-content-start">
-                        {filterTraining(Trainings).map((item) => { if (item.tags.some(t => t.name.includes(i.name))) return <TrainingInfoCard key={item.ID} Training={item} /> })}
+                        {filterTraining(Trainings).map((item) => { if (item.tags.some(t => t.name.includes(i.name))) return <TrainingInfoCard key={item.ID} Training={item} 
+                        Completed={Trainings.some(_ => _.profile.some(p => p.email.includes(profile!.email)))} /> })}
                     </div>
                 </div>
             }) : <h4 className="blue-text opacity-75">Er zijn hier nog geen trainingen...</h4>}
--- ClientApp/src/pages/Training/TrainingInfoCard.tsx
+++ ClientApp/src/pages/Training/TrainingInfoCard.tsx
@@ -3,13 +3,16 @@ import ITraining from "../../interfaces/ITraining";
 import { Modal, Card, Badge} from "react-bootstrap";
 import "./Training.scss";
 
+const checkmark = require("../../assets/checkmark.png");
+
 
 interface Props {
   Training?: ITraining;
+  Completed: boolean;
 }
 
 
-const TrainingInfoCard = ({ Training }: Props) => {
+const TrainingInfoCard = ({ Training, Completed }: Props) => {
   const [showModal, setModelstate] = useState<boolean>();
   return (
     <>
@@ -40,6 +43,7 @@ const TrainingInfoCard = ({ Training }: Props) => {
         <Card.Header>
           <Card.Title>
             <h4>{Training?.title}</h4>
+            {(Completed == true) ? <img src={checkmark} alt="" /> : null}
           </Card.Title>
           <div className="d-flex Category gap-2 pb-2 ">
             {Training?.tags.map((i) => (
