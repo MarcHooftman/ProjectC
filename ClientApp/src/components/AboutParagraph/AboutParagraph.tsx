e1a61569478fc47faa0e104ee6acf4879c3438a3
MarcHooftman <124693465+MarcHooftman@users.noreply.github.com>
2023-12-22 11:52:07 +0100

    Responsive (#35)
    * Option to switch between using public backend or local backend. Frontend will run on 0.0.0.0.
    
    * update Seeder, enable cors
    
    * .
    
    * Update Profile.scss and Profile.tsx
    
    * cards blauw
    
    * Alles behalve admin kant responsive en cards blauw
    
    * .
    
    * fixed bug where navigating from page to page didn't scroll to top. changed 0.0.0.0 to localhost
    
    * Add information about Antes and its merger with Parnassia Groep

--- ClientApp/src/components/AboutParagraph/AboutParagraph.tsx
+++ ClientApp/src/components/AboutParagraph/AboutParagraph.tsx
@@ -4,10 +4,12 @@ const AboutParagraph = () => {
   return (
     <div className="about-paragraph-container">
       <p className="blue-text fs-5">
-        Wie wij zijn
         Antes is specialist in psychiatrie en verslaving.
         Antes richt zich als ggz-instelling op het herstel van volwassenen en ouderen met (ernstige) psychiatrische problemen.
         Het doel van Antes is mensen met psychische problemen de juiste zorg bieden, op het juiste moment, zo intensief als nodig maar niet langer dan nodig.
+
+        Antes (ontstaan uit een fusie met  fusie van Delta psychiatrisch centrum Poortugaal en Bouman GGZ) fuseerde in oktober 2017 met Parnassia Groep.
+        Als ketenzorgbedrijf heeft Antes in haar werkgebied een belangrijke rol in het realiseren van een goede afstemming en samenwerking tussen de zorgbedrijven van Parnassia Groep.
       </p>
     </div>
   );
