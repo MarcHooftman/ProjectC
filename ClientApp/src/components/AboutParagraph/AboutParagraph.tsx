f943e6c351bb4baf19573bb4d9c36596f03355f3
MarcHooftman <1064802@hr.nl>
2023-12-22 11:55:35 +0100

    Add line break in AboutParagraph component

--- ClientApp/src/components/AboutParagraph/AboutParagraph.tsx
+++ ClientApp/src/components/AboutParagraph/AboutParagraph.tsx
@@ -7,7 +7,7 @@ const AboutParagraph = () => {
         Antes is specialist in psychiatrie en verslaving.
         Antes richt zich als ggz-instelling op het herstel van volwassenen en ouderen met (ernstige) psychiatrische problemen.
         Het doel van Antes is mensen met psychische problemen de juiste zorg bieden, op het juiste moment, zo intensief als nodig maar niet langer dan nodig.
-
+        <br />
         Antes (ontstaan uit een fusie met  fusie van Delta psychiatrisch centrum Poortugaal en Bouman GGZ) fuseerde in oktober 2017 met Parnassia Groep.
         Als ketenzorgbedrijf heeft Antes in haar werkgebied een belangrijke rol in het realiseren van een goede afstemming en samenwerking tussen de zorgbedrijven van Parnassia Groep.
       </p>
