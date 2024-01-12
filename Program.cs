2743e39edfff018f310fb9ccd54bd77c293a9ded
MarcHooftman <1064802@hr.nl>
2024-01-12 15:06:32 +0100

    Update CORS configuration

--- Program.cs
+++ Program.cs
@@ -17,14 +17,20 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.Use(async (context, next) =>
-{
-    context.Response.Headers.Add("Access-Control-Allow-Origin", "https://localhost:44463");
-    context.Response.Headers.Add("Access-Control-Allow-Origin", "https://antesonboarding.vercel.app");
-    context.Response.Headers.Add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    context.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type");
-    await next.Invoke();
-});
+// app.Use(async (context, next) =>
+// {
+//     context.Response.Headers.Add("Access-Control-Allow-Origin", "https://localhost:44463");
+//     context.Response.Headers.Add("Access-Control-Allow-Origin", "https://antesonboarding.vercel.app");
+//     context.Response.Headers.Add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+//     context.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type");
+//     await next.Invoke();
+// });
+
+app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials()); // allow credentials
 
 app.Use(async (context, next) =>
 {
