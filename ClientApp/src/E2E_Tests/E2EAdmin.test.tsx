d48cccb589155efd1324412408ab9286807731b3
Ronald98kj <124693284+Ronald98kj@users.noreply.github.com>
2024-01-17 23:22:22 +0100

    Test Admin en TempUser
    Test Admin en TempUser gemaakt

--- ClientApp/src/E2E_Tests/E2EAdmin.test.tsx
+++ ClientApp/src/E2E_Tests/E2EAdmin.test.tsx
@@ -8,3 +8,67 @@ test /api/admin/{id} PUT
 test /api/admin/{id} DELETE
 
 */
+import IAdmin from "../interfaces/IAdmin";
+import { getApiUrl } from "../utils/getApiUrl";
+import { createMockAdmin, deleteMockAdmin } from "./utils";
+
+const TEST_ID = 123456;
+
+describe('Admin API endpoints', () => {
+    const fetchUrl = getApiUrl() + "/admin";
+
+    let testAdminId = -1;
+
+    beforeAll(async () => { testAdminId = await createMockAdmin(); });
+
+    afterAll(async () => {
+        await deleteMockAdmin(testAdminId);
+    });
+
+    test('POST /api/admin', async () => {
+        const admin: IAdmin = {
+            id: TEST_ID,
+            level: 1,
+            email: "testadmin@ad.min",
+            password: "testpassword",
+        };
+        const response = await fetch(fetchUrl, { 
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+                "ngrok-skip-browser-warning": "1",
+            },
+            body: JSON.stringify(admin) 
+        });
+        expect(response.status).toBe(201);
+    });
+    test('GET /api/admin', async () => {
+        const response = await fetch(fetchUrl, { method: 'GET', headers: { "ngrok-skip-browser-warning": "1" } });
+        expect(response.status).toBe(200);
+    });
+    test('GET /api/admin/{id}', async () => {
+        const response = await fetch(`${fetchUrl}/${TEST_ID}`, { method: 'GET', headers: { "ngrok-skip-browser-warning": "1" } });
+        expect(response.status).toBe(200);
+    });
+    test('PUT /api/admin/{id}', async () => {
+        const updatedAdmin: IAdmin = {
+            id: TEST_ID,
+            level: 1,
+            email: "testadmin@ad.min",
+            password: "testpassword",
+        };
+        const response = await fetch(`${fetchUrl}/${TEST_ID}`, { 
+            method: 'PUT', 
+            headers: {
+                'Content-Type': 'application/json',
+                "ngrok-skip-browser-warning": "1",
+            },
+            body: JSON.stringify(updatedAdmin) 
+        });
+        expect(response.status).toBe(204);
+    });
+    test('DELETE /api/admin/{id}', async () => {
+        const response = await fetch(`${fetchUrl}/${TEST_ID}`, { method: 'DELETE', headers: { "ngrok-skip-browser-warning": "1" } });
+        expect(response.status).toBe(204);
+    });
+});
--- ClientApp/src/E2E_Tests/E2ETempUser.test.tsx
+++ ClientApp/src/E2E_Tests/E2ETempUser.test.tsx
@@ -8,3 +8,68 @@ test /api/tempUser/{id} PUT
 test /api/tempUser/{id} DELETE
 
 */
+import ITempUser from "../interfaces/ITempUser";
+import { getApiUrl } from "../utils/getApiUrl";
+import { createMockTempUser, deleteMockTempUser } from "./utils";
+
+const TEST_ID = 123456;
+
+describe('TempUser API endpoints', () => {
+    const fetchUrl = getApiUrl() + "/tempUser";
+
+    let testTempUserId = -1;
+
+    beforeAll(async () => { testTempUserId = await createMockTempUser(); });
+
+    afterAll(async () => {
+        await deleteMockTempUser(testTempUserId);
+    });
+
+    test('POST /api/tempUser', async () => {
+        const tempUser: ITempUser = {
+            id: TEST_ID,
+            email: "tempuser@email.nl",
+            expirationDate: "2024-02-02",
+            verificationCode: "123456",
+        };
+        const response = await fetch(fetchUrl, { 
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+                "ngrok-skip-browser-warning": "1",
+            },
+            body: JSON.stringify(tempUser) 
+        });
+        expect(response.status).toBe(200);
+    });
+    test('GET /api/tempUser', async () => {
+        const response = await fetch(fetchUrl, { method: 'GET', headers: { "ngrok-skip-browser-warning": "1" } });
+        expect(response.status).toBe(200);
+    });
+    test('GET /api/tempUser/{id}', async () => {
+        const response = await fetch(`${fetchUrl}/${TEST_ID}`, { method: 'GET', headers: { "ngrok-skip-browser-warning": "1" } });
+        expect(response.status).toBe(200);
+    });
+    test('PUT /api/tempUser/{id}', async () => {
+        const updatedTempUser: ITempUser = {
+            id: TEST_ID,
+            email: "tempuser@email.nl",
+            expirationDate: "2024-04-02",
+            verificationCode: "123",
+        };
+        const response = await fetch(`${fetchUrl}/${TEST_ID}`, { 
+            method: 'PUT', 
+            headers: {
+                'Content-Type': 'application/json',
+                "ngrok-skip-browser-warning": "1",
+            },
+            body: JSON.stringify(updatedTempUser) 
+        });
+        expect(response.status).toBe(204);
+    });
+    test('DELETE /api/tempUser/{id}', async () => {
+        const response = await fetch(`${fetchUrl}/${TEST_ID}`, { method: 'DELETE', headers: { "ngrok-skip-browser-warning": "1" } });
+        expect(response.status).toBe(204);
+    });
+});
+
