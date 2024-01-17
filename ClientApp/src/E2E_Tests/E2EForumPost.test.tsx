3989291fc6d103d17d7a61d1d0a703ec4f457cc7
R.S.Hussain <124694812+RSHussain@users.noreply.github.com>
2024-01-17 21:00:09 +0100

    Forumpost E2E test (#43)
    * Fixed many to many relationship between Training and Profile. implemented filter for trainingen.
    
    * Deleted Migrations
    
    * in progress Training profile progress
    
    * Finished Tracking Progress Training page
    
    * Training is now in profile
    
    * .
    
    * migrations
    
    * Training update pin progress
    
    * Completed Training functionaltity
    
    * card header to card body
    
    * .
    
    * Training admin overview page.
    
    * changed media to just have a url
    
    * changed new functions according to new main changes
    
    * Admin training functionality
    
    * fixed admin add training test
    
    * idk
    
    * fixed adding tag bug
    
    * admin training navigate bug
    
    * did half of E2E test and changed seeder for training before presentation
    
    * .
    
    * changed .env
    
    * idk
    
    * admin training fix
    
    * fixed admin training tag input
    
    * E2E Tests
    
    * Forumpost E2E test
    
    ---------
    
    Co-authored-by: MarcHooftman <124693465+MarcHooftman@users.noreply.github.com>

--- ClientApp/src/E2E_Tests/E2EForumPost.test.tsx
+++ ClientApp/src/E2E_Tests/E2EForumPost.test.tsx
@@ -11,3 +11,98 @@ test /api/forumpost/{id} DELETE
 
 */
 
+import IForumPost from "../interfaces/IForumPost";
+import { getApiUrl } from "../utils/getApiUrl";
+import { createMockProfile, deleteAllMockInstances } from "./utils";
+
+const TEST_ID = 123456;
+
+describe('Forum API endpoints', () => {
+    const fetchUrl = getApiUrl() + "/forumpost";
+
+    let testProfileId = -1;
+
+    beforeAll(async () => {
+        const CREATED_PROFILE_ID = await createMockProfile();
+        testProfileId = CREATED_PROFILE_ID;
+    });
+
+    afterAll(async () => {
+        await deleteAllMockInstances({
+            profileId: testProfileId,
+        });
+    });
+
+    test('POST /api/forumpost', async () => {
+        const forum: IForumPost = {
+            id: TEST_ID,
+            title: "Test Forum",
+            content: "Test Content",
+            tags: [],
+            profileID: testProfileId,
+            time: new Date().toISOString(),
+            forumPostID: null,
+            comments: [],
+            likes: [],
+            reports: [],
+        };
+        const response = await fetch(fetchUrl, { 
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+                "ngrok-skip-browser-warning": "1",
+            },
+            body: JSON.stringify(forum) 
+        });
+        expect(response.status).toBe(201);
+    });
+
+    test('GET /api/forumpost', async () => {
+        const response = await fetch(fetchUrl, { method: 'GET', headers: { "ngrok-skip-browser-warning": "1" } });
+        expect(response.status).toBe(200);
+    });
+
+    test('GET /api/forumpost/popular', async () => {
+        const response = await fetch(`${fetchUrl}/popular`, { method: 'GET', headers: { "ngrok-skip-browser-warning": "1" } });
+        expect(response.status).toBe(200);
+    });
+
+    test('GET /api/forumpost/{id}', async () => {
+        const response = await fetch(`${fetchUrl}/${TEST_ID}`, { method: 'GET', headers: { "ngrok-skip-browser-warning": "1" } });
+        expect(response.status).toBe(200);
+    });
+
+    test('GET /api/forumpost/by-profile/{profileID}', async () => {
+        const response = await fetch(`${fetchUrl}/by-profile/${testProfileId}`, { method: 'GET', headers: { "ngrok-skip-browser-warning": "1" } });
+        expect(response.status).toBe(200);
+    });
+
+    test('PUT /api/forumpost/{id}', async () => {
+        const updatedForum: IForumPost = {
+            id: TEST_ID,
+            title: "Updated Test Forum",
+            content: "Updated Test Content",
+            tags: [],
+            profileID: testProfileId,
+            time: new Date().toISOString(),
+            forumPostID: null,
+            comments: [],
+            likes: [],
+            reports: [],
+        };
+        const response = await fetch(`${fetchUrl}/${TEST_ID}`, { 
+            method: 'PUT', 
+            headers: {
+                'Content-Type': 'application/json',
+                "ngrok-skip-browser-warning": "1",
+            }, 
+            body: JSON.stringify(updatedForum) 
+        });
+        expect(response.status).toBe(204);
+    });
+
+    test('DELETE /api/forumpost/{id}', async () => {
+        const response = await fetch(`${fetchUrl}/${TEST_ID}`, { method: 'DELETE', headers: { "ngrok-skip-browser-warning": "1" }});
+        expect(response.status).toBe(204);
+    });
+});
