515134f75a96ee6aa44f969e5e59b45fcce95742
MarcHooftman <1064802@hr.nl>
2024-01-17 22:01:16 +0100

    new ngrok url

--- ClientApp/src/utils/getApiUrl.tsx
+++ ClientApp/src/utils/getApiUrl.tsx
@@ -1,7 +1,7 @@
 function getApiUrl() {
     return process.env.REACT_APP_LOCAL_API == "true"
         ? "https://localhost:7185/api"
-        : "https://a427-2a02-a212-92c8-8400-5935-52f7-c522-7bd1.ngrok-free.app/api"
+        : "https://9fdf-2a02-a212-92c8-8400-1d71-9cfc-d50d-d6d.ngrok-free.app"
 }
 
 export { getApiUrl };
