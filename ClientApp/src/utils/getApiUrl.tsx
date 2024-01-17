5b8e35dde74f7f316bc42b2ef6a1e9ae40f2384d
MarcHooftman <1064802@hr.nl>
2024-01-17 22:05:43 +0100

    i am dumb

--- ClientApp/src/utils/getApiUrl.tsx
+++ ClientApp/src/utils/getApiUrl.tsx
@@ -1,7 +1,7 @@
 function getApiUrl() {
     return process.env.REACT_APP_LOCAL_API == "true"
         ? "https://localhost:7185/api"
-        : "https://9fdf-2a02-a212-92c8-8400-1d71-9cfc-d50d-d6d.ngrok-free.app"
+        : "https://9fdf-2a02-a212-92c8-8400-1d71-9cfc-d50d-d6d.ngrok-free.app/api"
 }
 
 export { getApiUrl };
