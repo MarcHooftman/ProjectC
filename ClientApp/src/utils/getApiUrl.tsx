81326d1806dc9b487ffe809fd658438f2bf49aa5
MarcHooftman <1064802@hr.nl>
2024-03-24 20:51:56 +0100

    api reboot

--- ClientApp/src/utils/getApiUrl.tsx
+++ ClientApp/src/utils/getApiUrl.tsx
@@ -1,7 +1,7 @@
 function getApiUrl() {
     return process.env.REACT_APP_LOCAL_API == "true"
         ? "https://localhost:7185/api"
-        : "https://d06a-2a02-a212-92c8-8400-fc7c-2abb-3c3d-d632.ngrok-free.app/api"
+        : "https://8999-2a02-a212-92c8-8400-4c55-337-fce3-2be2.ngrok-free.app/api"
 }
 
 export { getApiUrl };
