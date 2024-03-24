0c5eeebe66f1693c0c1dadad67a17b3f64218487
MarcHooftman <124693465+MarcHooftman@users.noreply.github.com>
2024-03-24 21:03:20 +0100

    Update getApiUrl.tsx

--- ClientApp/src/utils/getApiUrl.tsx
+++ ClientApp/src/utils/getApiUrl.tsx
@@ -1,7 +1,7 @@
 function getApiUrl() {
     return process.env.REACT_APP_LOCAL_API == "true"
         ? "https://localhost:7185/api"
-        : "https://b225-2a02-a212-92c8-8400-4c55-337-fce3-2be2.ngrok-free.app/api"
+        : "https://0a86-2a02-a212-92c8-8400-4c55-337-fce3-2be2.ngrok-free.app/api"
 }
 
 export { getApiUrl };
